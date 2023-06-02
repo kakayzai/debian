a2c52a9974e6dfcff4fab69ea4525654d608d1ec
kakayzai <134511733+kakayzai@users.noreply.github.com>
2023-06-02 13:58:14 +0000

    Add files via upload

---
+++ xfce.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+
+# Minimal XFCE Install
+
+# XFCE Core
+sudo apt install -y xfce4-appfinder 	#Application to quickly run applications and commands
+sudo apt install -y xfconf				#D-Bus-based configuration storage system
+sudo apt install -y xfdesktop4			#Configure the desktop background image, icons, launchers and folders
+sudo apt install -y thunar 				#The fast and easy to use file manager for the Xfce Desktop
+sudo apt install -y xfce4-panel			#Application launchers, window buttons, applications menu, workspace switcher and more
+sudo apt install -y xfce4-session		#Save the state of your desktop and restore it on the next startup
+sudo apt install -y xfce4-settings		#The Settings daemon which persists many Xfce settings
+sudo apt install -y libxfce4ui-utils	#Library used to share commonly used non-GTK+ utilities among the Xfce applications
+sudo apt install -y xfwm4 				#Handles the placement of windows on the screen
+#sudo apt install -y xfce4-power-manager	#Manage power sources and power consumption of devices
+#sudo apt install -y thunar-volman			#Automatic management of removable devices in Thunar
+#sudo apt install -y 
+#sudo apt install -y 
+
+    
+    
+    
+    
+# XFCE Applications
+sudo apt install -y mousepad 				# A simple text editor for Xfce
+sudo apt install -y catfish 				#a versatile file search utility for the Xfce desktop
+sudo apt install -y xfce4-screenshooter 	#Take screenshots of your Xfce desktop
+sudo apt install -y ristretto 				#Image viewer for the Xfce desktop
+sudo apt install -y xfce4-terminal			#Terminal emulator for the Xfce desktop
+#sudo apt install -y xfce4-taskmanager		#Easy to use task manager 
+#sudo apt install -y xfce4-screensaver		#A simple, secure screen saver and locker.
+#sudo apt install -y xfce4-panel-profiles	#A simple application to manage Xfce panel layouts
+#sudo apt install -y xfce4-notifyd			#Notification service for the Xfce desktop
+#sudo apt install -y parole					#Media player for the Xfce desktop
+#sudo apt install -y xfburn					#A simple CD/DVD burning tool.
+#sudo apt install -y orage					#A fast and easy to use graphical time-managing application
+#sudo apt install -y 
+#sudo apt install -y 
+#sudo apt install -y 
+#sudo apt install -y 
+#sudo apt install -y 
+
+
+
+
+# XFCE Plugins
+sudo apt install -y xfce4-pulseaudio-plugin 
+sudo apt install -y xfce4-whiskermenu-plugin
+#sudo apt install -y xfce4-battery-plugin
+#sudo apt install -y xfce4-clipman-plugin
+#sudo apt install -y xfce4-datetime-plugin
+#sudo apt install -y xfce4-notes-plugin
+#sudo apt install -y xfce4-weather-plugin
+#sudo apt install -y 
+#sudo apt install -y 
+#sudo apt install -y 
+#sudo apt install -y 
+
+    
